f78a53b97ff20127e9cb8f9a217060b59028abae
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2021-08-04 11:42:01 +0530

    deploy in to rinkeby network

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');// helps to connect with rinkeby newtork or anyother public network
+const Web3 = require('web3');
+
+const {interface, bytecode} =  require('./compile')
+const provider = new HDWalletProvider(
+    'labor decide cigar govern suit dirt drama twenty pipe humble video rival',
+    'https://rinkeby.infura.io/v3/7a04f5a9200041c8b82445299e76ea16'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({
+        data: bytecode,arguments: ['Hi there!!']
+    }).send({
+        gas:'1000000',from: accounts[0]
+    });
+
+    console.log('Contract deployed to ', result.options.address);
+}
+deploy();
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,7 +3,7 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());// provider which will help us to connect with Web3 to ETH netwok or any other network
 const { interface, bytecode } = require('../compile');//trying to the index object which is inside the contracts object which get created at time of compileing
-
+// interface is javascript ABI and bytecode is row compiled contracts 
 
 //****************************Sempile Test Example****************************//
 // class Car {
@@ -39,15 +39,30 @@ beforeEach(async () => {
 
     // Use One of those account to deploy
     // the contract
-    inbox = await new web3.eth.Contract(JSON.parse(interface)).deploy({
+    inbox = await new web3.eth.Contract(JSON.parse(interface))// interface contain the detail of the function of our contracts json objrct
+    .deploy({// deploy create the json object of the contracts 
         data: bytecode, arguments: ['Hi there!!']
-    }).send({ from: accounts[0],gas:'1000000' });
+    }).send({ // send is the function which deploys the contracts on the network 
+         from: accounts[0],gas:'1000000'
+    });
  
 });
 
 describe('Inbox', () => {
     it("Deploy a contract", () => {
-        console.log(inbox);
+        // console.log(inbox);
+        assert.ok(inbox.options.address);// assert.ok is checking for the if inbox.options.address has address of contract on the network
         // console.log(accounts)
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.ok(message, 'Hi there!!');
+    });
+
+    it('can change the message',async () => {
+        await inbox.methods.setMessage("Hello Bitch!!").send({ from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.ok(message, "Hello Bitch!!");
     });
 })
