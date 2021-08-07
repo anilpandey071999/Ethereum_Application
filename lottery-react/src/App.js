bb9f0e03f1b66f10e976eb129f4fff89def6098e
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2021-08-08 00:48:41 +0530

    ui of entering into lottery

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -4,20 +4,59 @@ import web3 from "./web3";
 import lottery from "./lottery";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    manager: "",
+    players: [],
+    balance: "",
+    value: "",
+    massage: "",
+  };
 
-    this.state = { manager: "" };
-  }
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
-    this.setState({ manager });
+    const players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+
+    this.setState({ manager, players, balance });
   }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ message: "Waiting on tranction success..." });
+
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether"),
+    });
+    this.setState({ message: "You have been entered!" });
+  };
+
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
-        <p>This contract is managed by {this.state.manager}</p>
+        <p>
+          This contract is managed by {this.state.manager}. There are currently{" "}
+          {this.state.players.length} people entered, competing to win{" "}
+          {web3.utils.fromWei(this.state.balance, "ether")} ether!
+        </p>
+        <hr />
+        <from onSubmit={this.onSubmit}>
+          <h4>Want try your luck?</h4>
+          <div>
+            <label>Amount of ether to enter </label>
+            <input
+              value={this.state.value}
+              onChange={(event) => this.setState({ valueL: event.target.value })
+              }
+            />
+          </div>
+          <button>ENTER</button>
+        </from>
+        <hr />
+        <h1>{this.state.message}</h1>
       </div>
     );
   }
