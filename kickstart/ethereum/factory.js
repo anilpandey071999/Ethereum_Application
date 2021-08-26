512e4092ecc89a334f0f16a82ecc358d6968fc53
anilpandey071999 <31802461+anilpandey071999@users.noreply.github.com>
2021-08-26 16:40:03 +0530

    adding route

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -1,9 +1,9 @@
-import web3 from './web3';
-import campaignFactory from './build/CampaignFactory.json';
+import web3 from "./web3";
+import campaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
-    JSON.parse(campaignFactory.interface),
-    '0xC57Dd6b7C819947dD83D4A93e593F2483A8768Fd'
+  JSON.parse(campaignFactory.interface),
+  "0xC57Dd6b7C819947dD83D4A93e593F2483A8768Fd"
 );
 
 export default instance;
--- kickstart/pages/campaigns/new.js
+++ kickstart/pages/campaigns/new.js
@@ -1,35 +1,50 @@
-import { event } from 'jquery';
-import React,{Component} from 'react';
-import { Form,Button, Input } from 'semantic-ui-react';
-import Layout from '../../components/Layout'
+import React, { Component } from "react";
+import { Form, Button, Input, Message } from "semantic-ui-react";
+import Layout from "../../components/Layout";
+import factory from "../../ethereum/factory";
+import web3 from "../../ethereum/web3";
 
 class CampaignNew extends Component {
-    state = {
-        minimumContribution: ''
-    };
-    onSubmit = (event) => {
-        event.preventDefault();
-    };
-    render() {
-        return(
-            <Layout>
-                <h3>Create Campaign!</h3>
-            <Form onSubmit = {this.onSubmit}>
-                <Form.Field>
-                    <label>Minimum Contribution</label>
-                    <Input 
-                    label="wei" 
-                    labelPosition='right'
-                    value={this.state.minimumContribution}
-                    onChange={event => this.setState({minimumContribution: event.target.value })}
-                    />
-                </Form.Field>
-                <Button primary>Create!</Button>
-            </Form>
-            </Layout>
-        );
-        
+  state = {
+    minimumContribution: "",
+    errMessage: "",
+    loading:false
+  };
+  onSubmit = async (event) => {
+    event.preventDefault();
+    this.setState({ loading: true ,  errMessage: "" });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods.createCampaign(this.state.minimumContribution).send({
+        from: accounts[0],
+      });
+    } catch (err) {
+      this.setState({ errMessage: err.message });
     }
+    this.setState({ loading: false });
+  };
+  render() {
+    return (
+      <Layout>
+        <h3>Create Campaign!</h3>
+        <Form onSubmit={this.onSubmit} error={this.state.errMessage}>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input
+              label="wei"
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={(event) =>
+                this.setState({ minimumContribution: event.target.value })
+              }
+            />
+          </Form.Field>
+          <Message error header='Oops!' content = {this.state.errMessage} />
+          <Button loading={this.state.loading} primary>Create!</Button>
+        </Form>
+      </Layout>
+    );
+  }
 }
 
 export default CampaignNew;
