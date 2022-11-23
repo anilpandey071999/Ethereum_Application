4f1a96cac5334d38a27c52f0b54080b65869041e
anil.p6 <anil.p@accubits.com>
2022-11-23 09:55:13 +0530

    Changed network

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -4,7 +4,7 @@ import campaignFactory from "./build/CampaignFactory.json";
 const instance = new web3.eth.Contract(
   JSON.parse(campaignFactory.interface),
   // "0x3cb9d59686294Cb1A68B2A37a9f6f688aEcB1125"
-  "0xa2a5Dd944c04865d1fa117a6B235619F8aE0F0A9"
+  "0x012e3708e14bD6d313A06a95047F8129f3e7dCc5"
 );
 
 export default instance;
